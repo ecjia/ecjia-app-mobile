bad4a153b9be0b9eaf0e147488ce556d054a71b2
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-07 15:08:20 +0800

    更新模板文件

---
+++ templates/admin/cycleimage_edit.dwt.php
@@ -0,0 +1,95 @@
+<?php defined('IN_ECJIA') or exit('No permission resources.');?>
+<!-- {extends file="ecjia.dwt.php"} -->
+<!-- {block name="footer"} -->
+
+<script type="text/javascript">
+	ecjia.admin.cycleimage_edit.init();
+</script>
+<!-- {/block} -->
+
+<!-- {block name="main_content"} -->
+<div>
+	<h3 class="heading">
+		<!-- {if $ur_here}{$ur_here}{/if} -->
+		<!-- {if $action_link} -->
+		<a class="data-pjax btn plus_or_reply" id="sticky_a" href="{$action_link.href}"><i class="fontello-icon-reply"></i>{$action_link.text}</a>
+		<!-- {/if} -->
+	</h3>
+</div>
+<div class="row-fluid">
+	<div class="span12">
+		<form class="form-horizontal" id="form-privilege" name="theForm" action="{$form_action}" method="post" enctype="multipart/form-data" >
+			<fieldset>
+				<div class="control-group formSep">
+					{if $rt.img_src eq ''}
+						<label class="control-label">{lang key='mobile::mobile.upload_img'}</label>
+						<div class="controls">
+							<div class="fileupload fileupload-new" data-provides="fileupload">
+								<input type="hidden" name="{$var.code}" />
+								<div class="fileupload-preview fileupload-exists thumbnail" style="width: 50px; height: 50px; line-height: 50px;"></div>
+								<span class="btn btn-file">
+									<span class="fileupload-new">{lang key='mobile::mobile.browse'}</span>
+									<span class="fileupload-exists">{lang key='mobile::mobile.modify'}</span>
+									<input type='file' name='img_file_src' size="35" />
+								</span>
+								<a class="btn fileupload-exists" data-dismiss="fileupload" href="#">{lang key='system::system.drop'}</a>
+								<span class="input-must">{lang key='system::system.require_field'}</span> 
+							</div>
+						</div>
+					{else}
+						<label class="control-label">{lang key='mobile::mobile.preview_img'}</label>
+						<div class="controls">
+							<div class="fileupload fileupload-new" data-provides="fileupload">
+								<img class="w600 h300"  class="img-polaroid" src="{RC_Upload::upload_url()}/{$rt.img_src}"><br><br>
+								{lang key='mobile::mobile.file_address'}{$rt.img_src}<br><br>
+								<input type="hidden" name="{$var.code}" />
+								<div class="fileupload-preview fileupload-exists thumbnail" style="width: 50px; height: 50px; line-height: 50px;"></div>
+								<span class="btn btn-file">
+									<span class="fileupload-new">{lang key='mobile::mobile.change_image'}</span>
+									<span class="fileupload-exists">{lang key='mobile::mobile.modify'}</span>
+									<input type='file' name='img_file_src' size="35" />
+								</span>
+								<a class="btn fileupload-exists" data-dismiss="fileupload" href="#">{lang key='system::system.drop'}</a>
+								<input type="hidden" name="{$var.code}" />
+								<input type="hidden" name="{$rt.img_src}" />
+								 <input name="img_src" value="{$rt.img_src}" class="hide">
+							</div>
+						</div>
+					{/if}
+				</div>
+
+				<div class="control-group formSep">
+					<label class="control-label">{lang key='mobile::mobile.label_img_url'}</label>
+					<div class="controls">
+						<input class="span8" name="img_url" type="text" value="{if $smarty.get.ad_link}{$smarty.get.ad_link}{else}{$rt.img_url}{/if}" size="40" />
+						<span class="input-must">{lang key='system::system.require_field'}</span> 
+					</div>
+				</div>
+				<div class="control-group formSep" >
+					<label class="control-label">{lang key='mobile::mobile.label_img_text'}</label>
+					<div class="controls">
+						<textarea class="span8" name="img_text" cols="40" rows="3">{$rt.img_txt}</textarea>
+					</div>
+				</div>
+				<div class="control-group formSep">
+					<label class="control-label">{lang key='mobile::mobile.label_img_sort'}</label>
+					<div class="controls">
+						<input class="span8" name="img_sort" type="text" value="{$rt.img_sort}" size="5" maxlength="4"/>
+					</div>
+				</div>
+				<div class="control-group">
+					<div class="controls">
+						<input type="hidden"  name="id"		value="{$rt.id}" />
+						<input type="hidden"  name="step"	value="2" />
+						{if $rt.id eq ''}
+						<button class="btn btn-gebo" type="submit">{lang key='system::system.button_submit'}</button>
+						{else}
+						<button class="btn btn-gebo" type="submit">{lang key='mobile::mobile.update'}</button>
+						{/if}
+					</div>
+				</div>
+			</fieldset>
+		</form>
+	</div>
+</div>
+<!-- {/block} -->
