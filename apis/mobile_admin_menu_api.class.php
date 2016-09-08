165385d7dedab209190d968303c90e9ff77274a6
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-08 09:20:42 +0800

    更新应用

--- apis/mobile_admin_menu_api.class.php
+++ apis/mobile_admin_menu_api.class.php
@@ -12,15 +12,14 @@ class mobile_admin_menu_api extends Component_Event_Api {
         
         $submenus = array(
             ecjia_admin::make_admin_menu('01_mobile_list', RC_Lang::get('mobile::mobile.shorcut'), RC_Uri::url('mobile/admin_shortcut/init'), 1)->add_purview('shortcut_manage'),
-        	ecjia_admin::make_admin_menu('02_cycleimage_list', RC_Lang::get('mobile::mobile.mobile_cycleimage'), RC_Uri::url('mobile/admin_cycleimage_phone/init'), 2)->add_purview('cycleimage_manage'),
-        	ecjia_admin::make_admin_menu('03_discover_list', RC_Lang::get('mobile::mobile.discover'), RC_Uri::url('mobile/admin_discover/init'), 3)->add_purview('discover_manage'),
-        	ecjia_admin::make_admin_menu('04_device_list', RC_Lang::get('mobile::mobile.mobile_device'), RC_Uri::url('mobile/admin_device/init'), 4)->add_purview('device_manage'),
-        	ecjia_admin::make_admin_menu('05_mobile_news', RC_Lang::get('mobile::mobile.mobile_news'), RC_Uri::url('mobile/admin_mobile_news/init'), 5)->add_purview('mobile_news_manage'),
-        	ecjia_admin::make_admin_menu('06_mobile_toutiao', RC_Lang::get('mobile::mobile.mobile_headline'), RC_Uri::url('mobile/admin_mobile_toutiao/init'), 6)->add_purview('mobile_news_manage'),
-        	ecjia_admin::make_admin_menu('07_mobile_activity', RC_Lang::get('mobile::mobile.app_activity_list'), RC_Uri::url('mobile/admin_mobile_activity/init'), 7)->add_purview('mobile_activity_manage'),
-        	ecjia_admin::make_admin_menu('divider', '', '', 8)->add_purview('mobile_config_manage', 8),
-        	ecjia_admin::make_admin_menu('08_config', RC_Lang::get('mobile::mobile.mobile_config'), RC_Uri::url('mobile/admin_config/init'), 9)->add_purview('mobile_config_manage'),
-        	ecjia_admin::make_admin_menu('09_mobile_manage', RC_Lang::get('mobile::mobile.mobile_manage'), RC_Uri::url('mobile/admin_mobile_manage/init'), 10)->add_purview('mobile_push_config_manage')
+        	ecjia_admin::make_admin_menu('02_discover_list', RC_Lang::get('mobile::mobile.discover'), RC_Uri::url('mobile/admin_discover/init'), 2)->add_purview('discover_manage'),
+        	ecjia_admin::make_admin_menu('03_device_list', RC_Lang::get('mobile::mobile.mobile_device'), RC_Uri::url('mobile/admin_device/init'), 3)->add_purview('device_manage'),
+        	ecjia_admin::make_admin_menu('04_mobile_news', RC_Lang::get('mobile::mobile.mobile_news'), RC_Uri::url('mobile/admin_mobile_news/init'), 4)->add_purview('mobile_news_manage'),
+        	ecjia_admin::make_admin_menu('05_mobile_toutiao', RC_Lang::get('mobile::mobile.mobile_headline'), RC_Uri::url('mobile/admin_mobile_toutiao/init'), 5)->add_purview('mobile_news_manage'),
+        	ecjia_admin::make_admin_menu('06_mobile_activity', RC_Lang::get('mobile::mobile.app_activity_list'), RC_Uri::url('mobile/admin_mobile_activity/init'), 6)->add_purview('mobile_activity_manage'),
+        	ecjia_admin::make_admin_menu('divider', '', '', 8)->add_purview('mobile_config_manage', 7),
+        	ecjia_admin::make_admin_menu('07_config', RC_Lang::get('mobile::mobile.mobile_config'), RC_Uri::url('mobile/admin_config/init'), 8)->add_purview('mobile_config_manage'),
+        	ecjia_admin::make_admin_menu('08_mobile_manage', RC_Lang::get('mobile::mobile.mobile_manage'), RC_Uri::url('mobile/admin_mobile_manage/init'), 9)->add_purview('mobile_push_config_manage')
         );
         
         $menus->add_submenu($submenus);
