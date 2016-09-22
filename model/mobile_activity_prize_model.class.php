210fb5d4fdf15344e4cc7f66d284f64750dda999
黄玉园 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-09-22 20:36:13 +0800

    app下model修改

--- model/mobile_activity_prize_model.class.php
+++ model/mobile_activity_prize_model.class.php
@@ -4,8 +4,6 @@ defined('IN_ECJIA') or exit('No permission resources.');
 class mobile_activity_prize_model extends Component_Model_Model {
 	public $table_name = '';
 	public function __construct() {
-		$this->db_config = RC_Config::load_config('database');
-		$this->db_setting = 'default';
 		$this->table_name = 'mobile_activity_prize';
 		parent::__construct();
 	}
