433edc6e74d87286160a34e4482b2a1b23c120a6
陈哲俊 <huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comjhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-11-09 18:56:22 +0800

    device信息查询修改提交

--- apis/mobile_device_info_api.class.php
+++ apis/mobile_device_info_api.class.php
@@ -18,16 +18,16 @@ class mobile_device_info_api extends Component_Event_Api {
 	    $user_id = isset($options['user_id']) ? $options['user_id'] : 0;
 	    if (isset($options['admin_id'])) {
 	        $user_id = $options['admin_id'];
-	        $is_admin = 1;
+	        $user_type = 'admin';
 	    } else {
-	        $is_admin = 0;
+	        $user_type = 'user';
 	    }
 	    
 	    if (!empty($user_id)) {
 // 	        $db = RC_Model::model('mobile/mobile_device_model');
 // 	        return $db->order(array('id' => 'DESC'))->find(array('user_id' => $user_id, 'is_admin' => $is_admin,  'device_code' => $options['device_code']));
 	        
-	        return RC_DB::table('mobile_device')->where('user_id', $user_id)->where('is_admin', $is_admin)->where('device_code', $options['device_code'])->first();
+	        return RC_DB::table('mobile_device')->where('user_id', $user_id)->where('user_type', $user_type)->where('device_code', $options['device_code'])->orderBy('id', 'desc')->first();
 	    }
         
 	}
