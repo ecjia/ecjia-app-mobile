6794ce4f1e93b489a29062a1c5436dba3d4bd57f
吴体方 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comthuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comfhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2016-12-16 22:06:23 +0800

    修改下载页面样式

--- templates/front/download.dwt.php
+++ templates/front/download.dwt.php
@@ -1,38 +1,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,minimal-ui" name="viewport">
+    <meta  name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0, minimal-ui">
     <title>{$page_title}</title>
     <style>
         {literal}
-        body{margin:0;padding:0;background-color: #148ac3;font-family: "微软雅黑";}
-        .pic{min-height: 250px;position: relative;}
-        .pic .icon{position: absolute; width: 24%;top: 34%;left: 38%;}
-		.pic p{font-size: 1em;text-align: center;position: absolute;top: 60%;width: 90%;margin: 0 5%;}
-        .btns{position: relative;}
-        .btns a {height: 17%;left: 15.6%;position: absolute;width: 68.8%; margin-top: -2px;}
-        .btns a.disabled {cursor: default;background: #148ac3;}
-        .btns img{border: 0 none;display: block;}
-        .android {top: 18.37%;}
-        .iphone {top: 42.86%;}
-        .ipad {top: 67.35%;}
-        .close {display:block;cursor:pointer;position:absolute;top:0;right:0;width:1.2em;height:1.2em;padding: 0 0 0.2em 0.2em;text-align:center;border-bottom-left-radius:1em;font-size:1.2em;color:#fff;background:#148ac3;text-decoration: none;}
+        body{margin:0; padding:0; font-family: "微软雅黑"; margin: 0 auto; max-width: 640px;}
+        .pic .icon{width: 24%; padding-top: 50%; padding-left: 40%;}
+		.pic p{font-size: 1.2em; text-align: left; margin: 0 5%; padding-top: 5%; padding-bottom: 5%; line-height: 1.6em; text-indent: 2em;}
+        .btns {background-color: #148ac3; height: auto; padding: 10px 0 35px 0;}
+        .btns a {width: 100%;}
+        .btns img{height: auto; width: 80%; margin: auto 10%; display: block; border: 0 none; margin-top: 35px;}
+        .close {display:block; cursor:pointer; position:absolute; top:0; right:0; width:1.2em; height:1.2em; padding: 0 0 0.2em 0.2em; 
+        	text-align:center; border-bottom-left-radius:1em; font-size:1.2em; color:#fff; background:#148ac3; text-decoration: none;}
         {/literal}
     </style>
 </head>
 <body>
-    <div class="pic">
-        <img width="100%" src="{$front_url}/image/download_bj.png">
+    <div class="pic" style="background: url('{$front_url}/image/download_bj.png') 45% 0 no-repeat; min-height: 400px; background-size: 150%;">
         <div class="icon">
-            <img width="100%" src="{$shop_app_icon}" />
+        	<img width="100%" src="{$shop_app_icon}" />
         </div>
-        <p>{$shop_app_description}</p>
+        <div><p>{$shop_app_description}</p></div>
         <a class="close" href="{$shop_url}">x</a>
     </div>
+    {if $shop_android_download neq '' || $shop_iphone_download neq '' || $shop_ipad_download neq ''}
     <div class="btns">
-        <img src="{$front_url}/image/download_url.png" width="100%">
-        {if $shop_android_download eq ''}<a class="android downloadBtns disabled" href="javascript:;"></a>{else}<a class="android downloadBtns" href="{$shop_android_download}"></a>{/if}
-        {if $shop_iphone_download eq ''}<a class="iphone downloadBtns disabled" href="javascript:;"></a>{else}<a class="iphone downloadBtns" href="{$shop_iphone_download}"></a>{/if}
-        {if $shop_ipad_download eq ''}<a class="ipad downloadBtns disabled" href="javascript:;"></a>{else}<a class="ipad downloadBtns" href="{$shop_ipad_download}"></a>{/if}
+        {if $shop_android_download neq ''}<a class="android downloadBtns" href="{$shop_android_download}"><img src="{$front_url}/image/android.png"/></a>{/if}
+        {if $shop_iphone_download neq ''}<a class="iphone downloadBtns" href="{$shop_iphone_download}"><img src="{$front_url}/image/iphone.png"/></a>{/if}
+        {if $shop_ipad_download neq ''}<a class="ipad downloadBtns" href="{$shop_ipad_download}"><img src="{$front_url}/image/ipad.png"/></a>{/if}
     </div>
+    {/if}
 </body>
