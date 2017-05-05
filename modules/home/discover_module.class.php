7998eb781d031af34277858f438313e3807e5062
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2017-05-05 16:41:09 +0800

    清理注释代码

--- modules/home/discover_module.class.php
+++ modules/home/discover_module.class.php
@@ -52,25 +52,8 @@ defined('IN_ECJIA') or exit('No permission resources.');
  */
 class discover_module extends api_front implements api_interface {
 
-	    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
-    	$this->authSession();
-		//检测是否有用户登陆状态
-		
-// 		$mobile = RC_Loader::load_app_class('mobile_method','mobile');
-// 		$discover_data = array();
-// 		$mobile_menu   = array_merge($mobile->discover_data(true));
-// 		if (!empty($mobile_menu)) {
-// 			foreach ($mobile_menu as $val) {
-// 				if ($val['display'] == '1') {
-// 					$discover_data[] = array(
-// 						'image'	=> $val['src'],
-// 						'text'	=> $val['text'],
-// 						'url'	=> $val['url']
-// 					);
-// 				}
-// 			}
-// 		}
-		
+    public function handleRequest(\Royalcms\Component\HttpKernel\Request $request) {	
+	
 		$request = royalcms('request');
 		
 		$city_id	= $request->input('city_id', 0);
